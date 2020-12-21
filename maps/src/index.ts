464343cf3f3700f1ebec2ee791fb038ac3db52b1
kold <talininkold@gmail.com>
2020-12-21 22:48:03 +0700

    maps: addMarker function

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,11 +1,13 @@
-// import { User } from './User'
-// import { Company } from './Company'
+import { User } from './User'
+import { Company } from './Company'
 import { CustomMap } from './CustomMap'
 
-// const user = new User()
-// const company = new Company()
+const user = new User()
+const company = new Company()
 
-new CustomMap('map')
+const map = new CustomMap('map')
+map.addMarker(user)
+map.addMarker(company)
 
 // console.log(user)
 // console.log(company)
