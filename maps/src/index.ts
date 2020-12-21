846cd6ef03f7b10a753a8faed0d5c04add0f8a62
kold <talininkold@gmail.com>
2020-12-21 21:45:53 +0700

    maps: CustomMap class for creating Google map

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,8 +1,11 @@
-import { User } from './User'
-import { Company } from './Company'
+// import { User } from './User'
+// import { Company } from './Company'
+import { CustomMap } from './CustomMap'
 
-const user = new User()
-const company = new Company()
+// const user = new User()
+// const company = new Company()
 
-console.log(user)
-console.log(company)
+new CustomMap('map')
+
+// console.log(user)
+// console.log(company)
